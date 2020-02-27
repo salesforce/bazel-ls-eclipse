3caa739f5daf6665e85fb04e2f69c13181837e00
Kiryl Kazakevich <kiryl.kazakevich@gmail.com>
2020-02-27 13:39:47 +0300

    Add B2Eclipse JDT.LS Test module (#16)
    * (#13) Add B2Eclipse JDT.LS Test module
    
    * (#13) Add B2Eclipse JDT.LS Test module
    
    * (#13) Add B2Eclipse JDT.LS Test module
    
    * (#13) Add B2Eclipse JDT.LS Test module

---
+++ com.salesforce.b2eclipse.tests/src/main/java/com/salesforce/b2eclipse/B2eclipseTestPlugin.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package com.salesforce.b2eclipse;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class B2eclipseTestPlugin implements BundleActivator {
+
+	public static final String PLUGIN_ID = "com.salesforce.b2eclipse.tests";
+
+	/* (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
