a657e34134d80d31e4a6419a97958306fd56d6d4
Pavel Mitrafanau <pavel_mitrafanau@epam.com>
2020-03-02 15:06:14 +0300

    Decouple from equinox version in the process connection provider

--- com.salesforce.b2eclipse.ui/src/main/java/com/salesforce/b2eclipse/ui/ProcessStreamConnectionProvider.java
+++ com.salesforce.b2eclipse.ui/src/main/java/com/salesforce/b2eclipse/ui/ProcessStreamConnectionProvider.java
@@ -5,6 +5,8 @@ import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,8 @@ import org.eclipse.core.runtime.Platform;
 public class ProcessStreamConnectionProvider extends org.eclipse.lsp4e.server.ProcessStreamConnectionProvider {
 	
 	public ProcessStreamConnectionProvider(String workingDir) {
+		String equinoxLauncherName = findEquinoxLauncherName(workingDir);
+		
 		List<String> commands = new ArrayList<>();
 		commands.add("java");
 		commands.add("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044");
@@ -26,7 +30,7 @@ public class ProcessStreamConnectionProvider extends org.eclipse.lsp4e.server.Pr
 		commands.add("-Xmx1G");
 		commands.add("-jar");
 		//change org.eclipse.equinox.launcher version for current
-		commands.add("./plugins/org.eclipse.equinox.launcher_1.5.700.v20200207-2156.jar");
+		commands.add("./plugins/" + equinoxLauncherName);
 		commands.add("-configuration");
 		if (Platform.getOS().equals(Platform.OS_WIN32)) {
 			commands.add("./config_win");
@@ -51,6 +55,24 @@ public class ProcessStreamConnectionProvider extends org.eclipse.lsp4e.server.Pr
 		setWorkingDirectory(new Path(workingDir).toOSString());
 	}
 	
+	private String findEquinoxLauncherName(String workingDir) {
+		try {
+			return Files.walk(Paths.get(workingDir))
+				.filter(Files::isRegularFile)
+				.filter(f -> f.getFileName().toString().contains("org.eclipse.equinox.launcher_"))
+				.findFirst()
+				.orElseThrow(() -> {
+					B2EPlugin.logError("Can't find equinox launcher executable", null);
+				    return new RuntimeException();
+				})
+				.getFileName()
+				.toString();
+		} catch (Exception e) {
+			B2EPlugin.logError(e);
+			throw new RuntimeException();
+		}
+	}
+
 	@Override
 	public InputStream getInputStream() {
 		return new FilterInputStream(super.getInputStream()) {
