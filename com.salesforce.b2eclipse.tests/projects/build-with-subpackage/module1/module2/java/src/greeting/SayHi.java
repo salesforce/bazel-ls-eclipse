c7437467a8bd96a1fb1d2e18b3ff4ead19936875
Ihar Yasko <ihar_yasko@epam.com>
2020-04-21 08:57:45 +0300

    Changed test project for build with subpackage test case

--- com.salesforce.b2eclipse.tests/projects/build-with-subpackage/module1/module2/java/src/greeting/SayHi.java
+++ com.salesforce.b2eclipse.tests/projects/build-with-subpackage/module1/module2/java/src/greeting/SayHi.java
@@ -1,12 +1,7 @@
-package sayhi;
-
-import log.Logger;
+package greeting;
 
 public class SayHi {
-
     public static void sayHi() {
-        Logger.logDebug("SayHi.sayHi");
         System.out.println("Hi!");
     }
-
 }
