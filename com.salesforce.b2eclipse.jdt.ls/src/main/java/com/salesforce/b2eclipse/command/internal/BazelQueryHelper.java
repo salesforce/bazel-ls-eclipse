9ccfbd7803b6b10c2cae6d60589cda73ae85c666
alexei shmidman <alexeishmidman@gmail.com>
2021-03-19 10:35:21 +0300

    Run bazel queries under Linux
    Fix behavior under Linux. 3 cases should be takes into account:
    1. Bazel query under Windows from both command line and
    java.lang.Process
    2. Bazel query under Linux from command line
    3. Bazel query under Linux from java.lang.Process

--- com.salesforce.b2eclipse.jdt.ls/src/main/java/com/salesforce/b2eclipse/command/internal/BazelQueryHelper.java
+++ com.salesforce.b2eclipse.jdt.ls/src/main/java/com/salesforce/b2eclipse/command/internal/BazelQueryHelper.java
@@ -162,16 +162,29 @@ public class BazelQueryHelper {
             throws IOException, InterruptedException, BazelCommandLineToolConfigurationException {
 
         ImmutableList.Builder<String> argBuilder = ImmutableList.builder();
+        final String osName = System.getProperty("os.name");
+        StringBuilder javaProjectIndicators = new StringBuilder();
+        javaProjectIndicators.append("kind('");
+        javaProjectIndicators.append(String.join("|", BazelBuildFileHelper.JAVA_PROJECT_INDICATORS));
+        javaProjectIndicators.append("', //...)");
 
         argBuilder.add("query");
-        argBuilder.add("\"kind(\'" + String.join("|", BazelBuildFileHelper.JAVA_PROJECT_INDICATORS) + "\', //...)\"");
+        // 3 types of behavior: 1. Windows (command line and java.lang.Process), 2. non-Windows command line, 3. non-Windows java.lang.Process
+        if (osName != null && osName.toLowerCase().startsWith("win")) {
+            argBuilder.add("\"" + javaProjectIndicators.toString() + "\"");
+        } else {
+            argBuilder.add(javaProjectIndicators.toString());
+        }
         argBuilder.add("--output");
         argBuilder.add("package");
 
         return bazelCommandExecutor.runBazelAndGetOutputLines(bazelWorkspaceRootDirectory, //
                 progressMonitor, //
                 argBuilder.build(), //
-                (t) -> (t == null || t.trim().isEmpty()) ? null : t.trim());
+                (t) -> {
+                    String line = (t == null || t.trim().isEmpty()) ? null : t.trim();
+                    return line;
+                });
     }
 
 }
